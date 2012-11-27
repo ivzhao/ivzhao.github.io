26b4d7eb69fec9c36ffa473d89316e7f37afb3c9
Ivan Zhao <ninifat@Future-Book.local>
2012-11-27 02:00:28 -0800

    New site mostly done.

--- script.js
+++ script.js
@@ -1,55 +1,36 @@
 $(document).ready(function(){
 
-    $('#about, #projects li').live('mouseenter mouseleave', function(e) {
-        var link = $(this).find('.text-link');
+  $('.thumb').on('mouseenter', function(e) {
+    $(this).addClass('entered');
+    var poptip = $(this).data('poptip');
+    if (poptip) $(this).append('<div class="poptip">' + poptip + '</div>');
+  });
 
-        var duration, textColor, linkColor;
-        if (e.type == 'mouseenter') {
-            duration = 100;
-            textColor = '#111';
-            linkColor = '#FF5F4D';
-            linkDecoration = 'underline';
-        } else {
-            duration = 240;
-            textColor = '#BBB';
-            linkColor = (link.closest('em').length > 0) ? '#111' : '#BBB';
-            linkDecoration = 'none';
-        }
+  $('.thumb').on('mouseleave', function(e) {
+    $(this).removeClass('entered');
+    $(this).find('.poptip').remove();
+  });
 
-        $(this).animate({'color': textColor}, duration, 'linear');
-        link.animate({'color': linkColor}, duration, 'linear');
-        link.css('text-decoration', linkDecoration);
-    })
+  $('a').on('mouseenter mouseleave', function(e){
+    var thumbClass = $(this).data('thumb-class');
+    if (thumbClass) $('.' + thumbClass).trigger(e.type);
+  });
 
-    $('.thumb').bind('mouseenter mouseleave', function(e) {
-        var duration = (e.type == 'mouseenter') ? 160: 300;
-        var opacity = (e.type == 'mouseenter') ? 1.0: 0.0;
-        $(this).children('.tooltip').fadeTo(duration, opacity);
-        if (e.type == 'mouseleave') $(this).children('.tooltip').css('display', 'none');
+  $.getJSON('http://twitter.com/status/user_timeline/ivanhzhao.json?count=1&callback=?', 
+    function(response){
+      if (response && response.length > 0) {
+        var lastTweet = response[0].text;
+        var timeParts = response[0].created_at.split(' ');
+        var timeDate = timeParts[1] + ' ' + timeParts[2];
+        $('#last_tweet').text(lastTweet);
+        $('#last_tweet_date').text(timeDate);
+      }
     });
 
-    $('.text-link').live('mouseenter mouseleave', function(e) {
-        var thumb = $('#'+$(this).attr('id')+'_thumb');
-        var positionTop = (e.type == 'mouseenter') ? 1 : 0;
-        thumb.trigger(e.type);
-        thumb.children('img').css('top', positionTop);
-    });
-
-    $.getJSON('http://twitter.com/status/user_timeline/ivanhzhao.json?count=1&callback=?', 
-        function(response){
-            if (response && response.length > 0) {
-                var lastTweet = response[0].text;
-                var timeParts = response[0].created_at.split(' ');
-                var timeDate = timeParts[1] + ' ' + timeParts[2];
-                $('#last_tweet').text(lastTweet);
-                $('#last_tweet_date').text(timeDate);
-            }
-    });
-
-    WebFont.load({
-        custom: {
-            families: ['MinionProRegular', 'MinionProItalic'],
-            urls : ['./style.css']
-        }
-    });
+  WebFont.load({
+    custom: {
+      families: ['MinionProRegular', 'MinionProItalic'],
+    urls : ['./style.css']
+    }
+  });
 });
